79ed9fe2d03425730fe0abb53ad29a0f50dd7294
coderv8 <vannan20697@gmail.com>
2017-06-07 16:55:05 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -4,7 +4,7 @@ public class Matrix
 public static void main(String[] ar)
 {
     int i,j;
- int[][] a={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+ int[][] a={{1,2,3,4},{5,6,7,8},{9,1,1,2},{3,4,5,16}};
  for(i=0;i<4;i++)
  {
     for(j=0;j<4;j++) 
