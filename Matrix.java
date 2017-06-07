fd571579c6c173f002dc82ba74fdab54d50bf57f
coderv8 <vannan20697@gmail.com>
2017-06-07 16:54:21 +0530

    Create Matrix.java

---
+++ Matrix.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Matrix
+        {
+public static void main(String[] ar)
+{
+    int i,j;
+ int[][] a={{1,2,3,4},{5,6,7,8},{9,10,11,12},{13,14,15,16}};
+ for(i=0;i<4;i++)
+ {
+    for(j=0;j<4;j++) 
+    {
+        System.out.print(a[i][j]+" ");
+    }
+    System.out.println();
+ }
+}
+}
