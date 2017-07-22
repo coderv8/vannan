c26fffb66dcdbddf8f8bdc92210787236a1ae1d8
coderv8 <vannan20697@gmail.com>
2017-07-22 21:41:33 +0530

    Update Vowel.java

--- Vowel.java
+++ Vowel.java
@@ -3,11 +3,12 @@ import java.util.*;
 public class Vowel {
     public static void main(String[] args) 
     {
+	    System.out.println("Enter the character : ");
         Scanner s=new Scanner(System.in);
-		char a=s.next().charAt(0);
-                if((a=='a') || (a=='e') ||(a=='i') || (a=='o') || (a=='u'))
-                    System.out.println("vowles");
+		char char1=s.next().charAt(0);
+                if((char1=='a') || (char1=='e') ||(char1=='i') || (char1=='o') || (char1=='u'))
+                    System.out.println("Entered character is an vowel");
                 else
-                    System.out.println("consonants");
+                    System.out.println("Entered character is an consonant");
     }
 }
