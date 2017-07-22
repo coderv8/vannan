bbadc9021eff4501b42cdc650eb256634df42242
coderv8 <vannan20697@gmail.com>
2017-07-22 21:38:04 +0530

    Update OddEven.java

--- OddEven.java
+++ OddEven.java
@@ -4,10 +4,10 @@ class OddEven
 {
 	public static void main (String[] args)
 	{
-		int a;
+		int inp;
 		Scanner s=new Scanner(System.in);
-		a=s.nextInt();
-		if(a%2==0)
+		inp=s.nextInt();
+		if(inp%2==0)
 		System.out.print("even");
 		else
 		System.out.print("odd");
