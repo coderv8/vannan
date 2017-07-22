082b634a238fcec21e18070116f6bb74793623b6
coderv8 <vannan20697@gmail.com>
2017-07-22 21:44:16 +0530

    Update LargestNumber.java

--- LargestNumber.java
+++ LargestNumber.java
@@ -4,7 +4,8 @@ public class Char
 {
     public static void main(String[] ar)
     {
-         int t,i;
+         int i;
+        System.out.println("Enter three numbers : ");
         Scanner s=new Scanner(System.in);
         int[] a=new int[3];
         for(i=0;i<3;i++)
